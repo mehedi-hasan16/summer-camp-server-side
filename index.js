d0295fb3556f3c82e7a775036a2b407da0f2f9dc
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-06-07 16:06:41 +0600

    added get methord

--- index.js
+++ index.js
@@ -14,59 +14,65 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const classCollection = client.db("languageCamp").collection("classes");
-    const usersCollection = client.db("languageCamp").collection("users");
+        const classCollection = client.db("languageCamp").collection("classes");
+        const usersCollection = client.db("languageCamp").collection("users");
+        const instructorsCollection = client.db("languageCamp").collection("instructors");
 
-    app.get('/classes', async(req, res)=>{
-        const result = await classCollection.find().toArray();
-        res.send(result)
-    })
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray();
+            res.send(result)
+        })
 
 
-//users 
-app.post('/users', async(req, res)=>{
-    const user = req.body;
-    const query = {email : user.email}
-    const existingUser = await usersCollection.findOne(query)
-    if(existingUser){
-        return res.send({message: 'user already exist'})
-    }else{
-        const result = await usersCollection.insertOne(user)
-        res.send(result)
+        //users 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
+            } else {
+                const result = await usersCollection.insertOne(user)
+                res.send(result)
+            }
+        })
+
+        // instructors
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorsCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-})
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('language-quest-camp-server-running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`language quest camp server is running on port ${port}`);
 })
